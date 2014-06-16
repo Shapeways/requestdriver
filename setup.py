67acfe72ed5f3373e0b626145f1c3520ee5fdaaa
justin <justin@shapeways.com>
2014-06-16 17:04:54 -0400

    changed author and email and fixed single/double quotes mix

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="requestdriver",
-    version='0.0.1',
-    author='Justin Iso',
-    author_email="justin@shapeways.com",
+    name='requestdriver',
+    version='0.1.0',
+    author='Shapeways',
+    author_email='api@shapeways.com',
     packages=find_packages(),
     install_requires=[
         'requests==2.2.1',
     ],
     setup_requires=[],
-    description="Python requests wrapper for making session-based requests with some useful methods",
+    description='Python requests wrapper for making session-based requests with some useful methods',
     url='http://gitlab.nyc.shapeways.net/quality/requestdriver',
 )
 
